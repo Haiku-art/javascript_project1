69a2df5380f66517db35d4ace0921d24c225dd76
Haiku-art <73226863+Haiku-art@users.noreply.github.com>
2022-11-02 20:21:14 +0200

    Add files via upload

--- myscript.js
+++ myscript.js
@@ -6,6 +6,8 @@ var toDo = JSON.parse(localStorage.getItem("to do")); //asettaa listan local sto
     lataaLista(); //kutsutaan funktiota joka lataa local storageen tallennetun listan. 
     }
 
+taustaVari() //hakee tallennetun taustavärin local storagesta
+
 document.getElementById('addBtn').addEventListener('click', lisaaListalle); //kuuntelija add-napille
 function lisaaListalle(){ //funktio ottaa tekstikentän "task" arvon (value) 
     let listalle = document.getElementById('task').value;
@@ -36,16 +38,11 @@ function addItem(i) {
     localStorage.setItem("to do", JSON.stringify(toDo)); //tallennetaan local storageen
   
     lataaLista2()
-
-    
 }
 
 function lataaLista(){
 
-   //localStorage["to do"] = JSON.stringify(toDo);
-   //var listaMuistista = stored_datas = JSON.parse(localStorage["to do"]);
    var listaMuistista  = JSON.parse(localStorage["to do"]);
-
   
    for (i = 0; i < listaMuistista.length; ++i) {
     var li = document.createElement('li');
@@ -55,13 +52,10 @@ function lataaLista(){
 }
 }; 
 
-function lataaLista2(){
-
+function lataaLista2(){ //tässä on ajatusvirhe, joten koodiblokki toistuu kahdesti melkein identtisenä. lataalista() lataa vanhat tiedot local storagesta... 
+                        //ja lataalista2() päivittää listaa sitä mukaan, kun lisäyksiä tehdään
 localStorage["to do"] = JSON.stringify(toDo);
-
 var listaMuistista = stored_datas = JSON.parse(localStorage["to do"]);
-
-
 
 for (i = 0; i < listaMuistista.length; ++i) {
  var li = document.createElement('li');
@@ -78,105 +72,13 @@ document.getElementById("lista2").addEventListener("click", function(event) { //
     event.target.style.color = 'grey'; //muokkaa listalta klikatun elementin ulkonäköä. 
   }
 }); 
-      
 
-
-document.getElementById('deleteList').addEventListener('click', poistaLista);
-function poistaLista(){
-    localStorage.clear(); //tyhjentää local storagen
-    document.getElementById('lista2').innerHTML = ''; //tyhjentää elementit listalta
-}
-
-document.getElementById('deleteList').addEventListener("mouseover", annaVaroitus);
-document.getElementById('deleteList').addEventListener("mouseout", poistaVaroitus);
-
-function annaVaroitus(){
-document.getElementById('warning').innerHTML = "Delete all entries?";
-}
-
-function poistaVaroitus(){
-document.getElementById('warning').innerHTML = "";
-}
-
-
-
-/*
-var x = JSON.parse(localStorage.getItem("to do"));
-    if(x == null){ 
-        x = [];
-    } else{
-        lataaLista();
+document.getElementById("lista2").addEventListener("dblclick", function(event) { 
+    if (event.target.matches("li.item")) {  
+        event.target.remove();
     }
-    
-
-document.getElementById('addBtn').addEventListener('click', lisaaListalle); //kuuntelija add-napille
-function lisaaListalle(){ //funktio ottaa tekstikentän "task" arvon (value) 
-    let listalle = document.getElementById('task').value;
-    document.getElementById("task").value = '';//nollaa tekstinsyöttäkentän 
-
-        if (listalle == "" || listalle.length < 3){ // syötteen tarkistus. Ohjelma ei anna syottää tyhjää arvoa eikä lyhyempää kuin 3 merkkiä
-            
-            document.getElementById('errorMessage').innerHTML = "Liian lyhyt syöte!";
-            document.getElementById('task').style.backgroundColor = 'pink';
-  
-            
-        }
-        else {
-            addItem(listalle); // kun menee tarkistuksesta läpi, kutsuu funtiota addItem, jolle annetaan tallennettu arvo parametrinä.
-            document.getElementById('task').style.backgroundColor = 'white'; 
-            document.getElementById('errorMessage').innerHTML = "";
-        }
-};
-
-function addItem(i) {
-    var li = document.createElement('li'); // luo listaelementin ja tallentaa sen muuttujaan. 
-    li.appendChild(document.createTextNode(i)); //  Luodaan uusi elementti createElement-metodilla ja appendChild() -metodi lisää elementin listan loppuun, lähde: https://www.w3schools.com/jsref/met_node_appendchild.asp. 
-    li.className = 'item';
-    x.push(i);
-    localStorage.setItem("to do", JSON.stringify(x));
-  
-    lataaLista2()
-
-    
-}
-
-function lataaLista(){
-
-   localStorage["to do"] = JSON.stringify(x);
-   var listaMuistista = stored_datas = JSON.parse(localStorage["to do"]);
-
-  
-   for (i = 0; i < listaMuistista.length; ++i) {
-    var li = document.createElement('li');
-    li.className = 'item';
-    li.innerText = listaMuistista[i] ;
-    document.getElementById("lista2").appendChild(li);   
-}
-}; 
-
-function lataaLista2(){
-
-localStorage["to do"] = JSON.stringify(x);
-var listaMuistista = stored_datas = JSON.parse(localStorage["to do"]);
-
-
-for (i = 0; i < listaMuistista.length; ++i) {
- var li = document.createElement('li');
- li.className = 'item';
- li.innerText = listaMuistista[i] ;
-
-}   document.getElementById("lista2").appendChild(li);
-}; 
-
-
-document.getElementById("lista2").addEventListener("click", function(event) { // Jouduin tähän googlailemaan aika paljon apuja, lähde: https://www.w3schools.com/jsref/event_target.asp
-  if (event.target.matches("li.item")) {  // event.target palauttaa sen arvon, jota klikattiin: tässä takistetaan, onko luokan nimi oikea.
-    event.target.innerHTML = '<s>'+event.target.outerHTML+'</s>';
-    event.target.style.color = 'grey'; //muokkaa listalta klikatun elementin ulkonäköä. 
-  }
-}); 
+  }); 
       
-
 
 document.getElementById('deleteList').addEventListener('click', poistaLista);
 function poistaLista(){
@@ -195,4 +97,22 @@ function poistaVaroitus(){
 document.getElementById('warning').innerHTML = "";
 }
 
-*/
+
+var laatikko = document.getElementById('header'),
+colors = ['salmon', 'purple', 'orange', '#69DDC1',];
+
+    laatikko.onclick = function () { //hakee värejä taulukosta klikkauksien mukaan (shift ja push)
+    var color = colors.shift();
+    colors.push(color);
+
+    localStorage.setItem('bgcolor', color); //värin tallennus local storageen
+    laatikko.style.backgroundColor = color; 
+   
+    };
+
+function taustaVari(){
+    var valittuVari = localStorage.getItem('bgcolor'); 
+    document.getElementById('header').style.backgroundColor = valittuVari;
+
+}
+
